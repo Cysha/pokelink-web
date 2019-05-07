3cdf47bda740767bc3e48ae3254961f804ac3215
Daniel <dnoel-davies@viprsolutions.com>
2019-05-07 22:42:53 +0100

    Fix badges

--- assets/js/badges.js
+++ assets/js/badges.js
@@ -2,14 +2,15 @@ new Vue({
   el: '#badges',
   data: function () {
     return {
-      badges: {}
+      badges: []
     };
   },
   mounted: function () {
     var vm = this;
     client.setup(settings.port, 'badges-'+settings.currentUser+'-browser', settings.server, () => {})
       .on('player:trainer:updated', (data) => {
-        this.badges = collect(data.update.trainer.badges).map(function(badge) {
+        console.log(data)
+        this.badges = data.update.trainer.badges.map(function(badge) {
           var badgeObj = {};
           badgeObj.img = 'https://pokelink.cybershade.org/assets/sprites/badges/'+badge.name.toLowerCase()+'.png';
           badgeObj.label = badge.name+' Badge';
