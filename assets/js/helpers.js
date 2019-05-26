cba34fa67d57267d6e138697ad955b271184a125
xLink <xLink@cybershade.org>
2019-05-26 16:48:12 +0100

    moved pokemon-letsgo -> letsgo, added another case to the pokemon renamer, and fixed issue to basic-card theme

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -18,9 +18,11 @@ function transformPokemon(pokemon) {
   }
 
   // normalize name
-  pokemon.normalizeName = pokemon.speciesName.toLowerCase();
-  pokemon.normalizeName = pokemon.normalizeName.replace(/[^a-zA-Z0-9\♀]/g, '');
-  pokemon.normalizeName = pokemon.normalizeName.replace(/[\♀]/g, '-f'); // nidoran female
+  pokemon.normalizeName = pokemon.speciesName
+    .toLowerCase()
+    .replace(/[é]/g, 'e')
+    .replace(/[^a-zA-Z0-9\♀]/g, '')
+    .replace(/[\♀]/g, '-f'); // nidoran female
 
   // figure out which version of the filename we wanna use
   var filename = settings.pokeImg.useDexNumbers
