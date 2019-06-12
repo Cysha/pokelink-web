5b1a2a58932bc2b7c45beb98ebe656c6fefd16bb
xLink <xLink@cybershade.org>
2019-06-12 14:36:05 +0100

    fixed some basic styling, and pokeball theme

--- themes/pokeball/assets/js/party.js
+++ themes/pokeball/assets/js/party.js
@@ -1,14 +1,15 @@
 new Vue({
-  el: '#party',
-  data: {
-    connected: false,
-    loaded: false,
-    settings: {},
-    party: [],
-    players: {},
-    party_count: 0,
-    switchSpeed: 'switchMedium',
-    stats: ['hp', 'atk', 'def', 'spatk', 'spdef', 'spd'],
+  el: "#party",
+  data() {
+    return {
+      connected: false,
+      loaded: false,
+      settings: {},
+      party: [],
+      players: {},
+      party_count: 6,
+      switchSpeed: 'switchMedium',
+    }
   },
   created: function () {
     this.loaded = true
@@ -17,26 +18,23 @@ new Vue({
   mounted: function () {
     var vm = this;
     client.setup(settings.port, settings.currentUser+'-browser', settings.server, (username, party) => {
-      vm.connected = true;
-      vm.players[username] = party.map(function (pokemonWrapper) {
-          let mon = pokemonWrapper.pokemon;
-          if (mon == null) {
-              return false;
-          }
+        vm.connected = true;
+        vm.players[username] = party.map(function (pokemonWrapper) {
+            let mon = pokemonWrapper.pokemon;
+            if (mon == null) {
+                return false;
+            }
 
-          return transformPokemon(mon);
-      });
+            return transformPokemon(mon);
+        });
 
-      if (username == client.currentUser) {
-          vm.party = vm.players[username];
-          vm.party_count = vm.party.filter(function(value) { return typeof value == "object" }).length;
-      }
+        if (username == client.currentUser) {
+            vm.party = vm.players[username];
+            vm.party_count = vm.party.filter(function(value) { return typeof value == "object" }).length;
+        }
     })
     .on('player:trainer:updated', (payload) => { this.updateTrainerStuffs(payload)})
     ;
-  },
-  updated: function( ){
-    var vm = this;
   },
   methods: {
     updateTrainerStuffs (payload) {
@@ -48,47 +46,59 @@ new Vue({
 
     },
 
-    healthBarPercent: function (pokemon) {
-      if (pokemon.hp.max === pokemon.hp.current) {
-        return 100;
-      }
-
-      return (100/pokemon.hp.max) * pokemon.hp.current;
+    update( val ) {
     },
-    healthBarClass: function (pokemon) {
-        var percent = this.healthBarPercent(pokemon);
-
-        if (percent == 0) {
-            return 'progress-bar grey';
-        }
-        if (percent <= 25) {
-            return 'progress-bar red';
-        }
-        if (percent <= 50) {
-            return 'progress-bar yellow';
-        }
-
-        return 'progress-bar green';
-    },
-    isBorderColorType: function() {
-      return settings.pokeImg.typeColor == true;
-    },
-    getTypeColor: function(type) {
-      return settings.typeColors[type.toLowerCase()];
-    },
-    getStatusColor: function(type) {
-      return settings.statusColors[type.toLowerCase()];
-    },
-    stringToColour: function(str) {
-      var colorHash = new ColorHash({lightness: 0.5});
-      return colorHash.hex(str);
-    }
   },
   computed: {
-    moveIndexes () {
-      return [1,2,3,4].map((index) => {
-        return 'move' + index;
-      });
+    singleSlot () {
+      let params = new URLSearchParams(document.location.search.substring(1));
+      if (params.has('slot')) {
+        return true
+      }
+      return false
+    },
+    slotId () {
+      let availableSlots = [1,2,3,4,5,6]
+      let params = new URLSearchParams(document.location.search.substring(1));
+      if (params.has('slot') && availableSlots.includes(parseInt(params.get('slot')))) {
+        return params.get('slot') - 1
+      }
+      return 0
+    },
+    pokemonToShow () {
+      let params = new URLSearchParams(document.location.search.substring(1));
+
+      if (this.singleSlot === true) {
+        return [this.party[this.slotId]]
+      }
+
+      if (params.has('fromSlot') && params.has('slots')) {
+        return this.party.slice(
+          parseInt(params.get('fromSlot')) - 1,
+          parseInt(params.get('fromSlot')) - 1 + parseInt(params.get('slots'))
+        )
+      }
+
+      return this.party
+    },
+    showEmptySlots() {
+
+      if (this.singleSlot === true) {
+        return false;
+      }
+
+      if (params.has('fromSlot') && params.has('slots')) {
+        if (this.pokemonToShow.includes(false)) {
+          return true;
+        }
+        return false;
+      }
+
+      if (this.party_count !== 6) {
+        return true;
+      }
+
+      return true;
     }
   }
 });
