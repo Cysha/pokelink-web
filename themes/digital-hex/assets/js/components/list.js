7ee66eb8dd4c25993c28c77f64fd49e2e6143635
Daniel Noel-Davies <NoelDavies@cybershade.org>
2020-11-16 16:45:16 +0000

    Add digital hex to themes list

---
+++ themes/digital-hex/assets/js/components/list.js
@@ -0,0 +1,65 @@
+Vue.component( "List", {
+  template: `
+  <div style="display: none" :class="{ 'browser-connected' : true, 'darkMode': settings.theme.darkMode }" class="pokes">
+    <transition-group :name="switchSpeed" tag="div" :class="['pokemon__list', {'flipped': flipped === true}]" v-if="loaded">
+      <Pokemon v-for="( poke, idx ) in party" :key="poke.pid || ('slot' + (idx + 10000000000))" :pokemon="poke">
+      </Pokemon>
+    </transition-group>
+    <div class="no-connection" v-if="!connected">
+      <p>Waiting for successful connection to Pokélink...</p>
+      <p>Attempting to connect on port {{settings.port}}</p>
+    </div>
+  </div>
+`,
+  data() {
+    return {
+      connected: false,
+      loaded: false,
+      settings: {},
+      party: [],
+      players: {},
+      party_count: 0,
+      switchSpeed: 'switchMedium',
+      flipped: false
+    }
+  },
+  created: function () {
+    this.loaded = true
+    this.settings = window.settings;
+    this.flipped = !!params.get('flipped')
+  },
+  mounted: function () {
+    var vm = this;
+    client.setup(settings.port, settings.currentUser+'-browser', settings.server, (username, party) => {
+        vm.connected = true;
+        vm.players[username] = party.map(function (pokemonWrapper) {
+            let mon = pokemonWrapper.pokemon;
+            if (mon == null) {
+                return false;
+            }
+
+            return transformPokemon(mon);
+        });
+
+        if (username == client.currentUser) {
+            vm.party = vm.players[username];
+            vm.party_count = vm.party.filter(function(value) { return typeof value == "object" }).length;
+        }
+    })
+    .on('player:trainer:updated', (payload) => { this.updateTrainerStuffs(payload)})
+    ;
+  },
+  methods: {
+    updateTrainerStuffs (payload) {
+      if (window.settings.debug) {
+        console.log(`Trainer Update recieved for ${payload.username}`)
+        console.log(payload, window.settings)
+      }
+      if (payload.username !== settings.currentUser) return;
+
+    },
+
+    update( val ) {
+    },
+  }
+});
---
+++ themes/digital-hex/assets/js/components/pokemon.js
@@ -0,0 +1,200 @@
+Vue.component( "Pokemon", {
+  template: `
+    <div :style="mainStyle" :class="{ 'pokemon': true, 'isDead': isDead, 'isEmpty': !pokemonExists, 'loaded': loaded, 'staggered': settings.theme.staggered }">
+      <svg
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        width="250"
+        height="250"
+        viewbox="0 0 250 200"
+        style="padding:0px;"
+      >
+      <path
+        id="background"
+        class="slot__background"
+        fill="#252027"
+        stroke-width="0px"
+        style=""
+        d="M86.60254037844386 5L178.20508075688772 55L178.20508075688772 155L91.60254037844386 205L5 155L5 55Z"
+      >
+      </path>
+      <defs>
+        <clipPath id="hexagon">
+          <path
+            fill="transparent"
+            stroke="rgb(62 62 75)"
+            stroke-width="10px"
+            style=""
+            d="M86.60254037844386 5L178.20508075688772 55L178.20508075688772 155L91.60254037844386 205L5 155L5 55Z"
+            class="slot__border-background"
+          >
+          </path>
+        </clipPath>
+      </defs>
+      <path
+        id="clip"
+        class="slot__background-inner"
+        :fill="borderColor"
+        stroke-width="0px"
+        style="opacity:0.3;"
+        d="M91.28203230275508 29L155.56406460551017 64L155.56406460551017 137L89.28203230275508 177L29 141L29 61Z"
+      >
+      </path>
+    </svg>
+
+    <TrimmedSprite
+      v-if="typeof pokemon == 'object'"
+      :key="ident"
+      :pokemon="pokemon"
+      :maxBoundingBoxHeight="150"
+      @done="loaded = true"
+    ></TrimmedSprite>
+
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      width="250"
+      height="250"
+      viewbox="0 0 250 200"
+      style="padding:0px;"
+      class="slot__border-wrapper"
+    >
+    <path
+      fill="transparent"
+      stroke="rgb(62 62 75)"
+      stroke-width="10px"
+      style=""
+      d="M86.60254037844386 5L178.20508075688772 55L178.20508075688772 155L91.60254037844386 205L5 155L5 55Z"
+      class="slot__border-background"
+    >
+    </path>
+    <path
+      fill="transparent"
+      :stroke="borderColor"
+      stroke-dasharray="600 600"
+      stroke-width="10px"
+      stroke-linecap="butt"
+      :stroke-dashoffset="dashOffset"
+      d="M86.60254037844386 5L178.20508075688772 55L178.20508075688772 155L91.60254037844386 205L5 155L5 55Z"
+      class="slot__border"
+    >
+    </path>
+  </svg>
+  </div>
+  `,
+  props: {
+    pokemon: {},
+    key: {},
+    stroke: {
+      type: Number,
+      default () {
+        return 10
+      }
+    }
+  },
+  data () {
+    return {
+      settings: {},
+      loaded: false
+    }
+  },
+  created () {
+    this.settings = window.settings;
+  },
+  computed: {
+    pokemonExists () {
+      if (!this.pokemon || !this.pokemon.hasOwnProperty('hp')) return false
+      return true
+    },
+    dashOffset () {
+      if (this.pokemonExists === false ) { return false; }
+      if (this.settings.theme.hideHPBar) return 0;
+      return 600 - (this.healthPercent / 100 * 600)
+    },
+    healthPercent() {
+      if (this.pokemonExists === false ) { return null; }
+      return (100/this.pokemon.hp.max) * this.pokemon.hp.current
+    },
+    isDead () {
+      if (typeof this.pokemon === "undefined") { return false; }
+
+      return parseFloat(this.healthPercent) === 0
+    },
+    level () {
+      if (this.pokemonExists === false ) { return null; }
+      return this.pokemon.level || '0';
+    },
+    nickname() {
+      if (this.pokemonExists === false ) { return null; }
+      return this.pokemon.nickname || this.pokemon.speciesName;
+    },
+    sex() {
+      return (this.pokemon.isGenderless ? '' : (this.pokemon.isFemale ? 'female' : 'male'));
+    },
+    ident () {
+      if (this.pokemonExists === false ) { return null; }
+      return this.pokemon.pid;
+    },
+    opacity() {
+      if (typeof this.pokemon === "undefined") { return '0.4'; }
+      return '1';
+    },
+    hasItem() {
+      if (this.pokemonExists === false ) { return null; }
+      if (typeof this.pokemon === "undefined") { return false; }
+      if (typeof this.pokemon.heldItem === "undefined") { return false; }
+      if (typeof this.pokemon.heldItem.id === "undefined") { return false; }
+      return this.pokemon.heldItem.id !== 0;
+    },
+    sprite () {
+      console.log(this.pokemon)
+      if (typeof this.pokemon === "undefined") { return ''; }
+      if (this.pokemon.img) {
+        return this.pokemon.img;
+      }
+
+      return '';
+    },
+    mainStyle () {
+      if (this.pokemonExists === false ) { return null; }
+      let styles = {
+        // 'opacity': this.opacity,
+      }
+
+      if (this.pokemon) {
+        let primaryType = this.pokemon.types[0].label.toLowerCase()
+        styles = {...styles, }//'background-image': 'linear-gradient(180deg, ' + this.settings.typeColors[primaryType] + ', white)'}
+      }
+
+      return styles;
+    },
+
+    borderColor () {
+      if (this.pokemonExists === false ) { return '#7375ae'; }
+      let primaryType = this.pokemon.types[0].label.toLowerCase()
+      return this.settings.typeColors[primaryType]
+    },
+    nature () {
+      if (typeof this.pokemon === "undefined") { return ''; }
+      if (!this.pokemon.nature) return ''
+
+      return this.pokemon.nature.charAt(0).toUpperCase() + this.pokemon.nature.slice(1)
+    },
+
+    selectedPokemon: {
+      get: function() {
+        return this.nickname
+      },
+      set: function() {
+        this.$emit( "change", this.nickname )
+      }
+    }
+  },
+  watch: {
+    pokemon (oldPoke, newPoke) {
+      if (oldPoke.img !== newPoke.img) {
+        this.fixedSprite = false
+      }
+    }
+  }
+});
